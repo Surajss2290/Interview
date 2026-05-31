219ef070440e59fee58de3dc27cfb1609cabd363
Suraj Suryawanshi <surajsuryawanshi2290@gmail.com>
2026-05-31 19:20:15 +0530

    CICD Deployment

--- src/main/java/org/example/interview/Employee.java
+++ src/main/java/org/example/interview/Employee.java
@@ -1,42 +1,25 @@
 package org.example.interview;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+
+@Entity
 @Data
 public class Employee {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     String name;
     double salary;
 
 
-    public Employee( String name, double salary) {
-       // this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
 
-
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
